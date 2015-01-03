32b9cfa25fbd46e52aa33119a10ec8f226bef628
Lea Verou <lea@verou.me>
2015-01-03 20:04:57 +0200

    Added some style and a bit of initial codez

--- redirect.js
+++ redirect.js
@@ -1,1 +1,61 @@
-console.log(location.href);
+function $(expr, con) {
+	return typeof expr === 'string'? (con || document).querySelector(expr) : expr;
+}
+
+function $$(expr, con) {
+	return Array.prototype.slice.call((con || document).querySelectorAll(expr));
+}
+
+function xhr(o) {
+	var xhr = new XMLHttpRequest(o.src);
+	
+	xhr.open("GET", o.src);
+	
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4) {
+			if (xhr.status < 400) {
+				try {
+					o.onsuccess.call(xhr);
+				}
+				catch (e) {
+					o.onerror.call(xhr);
+					console.error(e);
+				}
+			}
+			else {
+				o.onerror.call(xhr);
+			}
+		}
+	};
+	
+	xhr.send();
+}
+
+(function(){
+
+document.body.className = 'redirecting';
+
+var slug = location.pathname.slice(1);
+
+xhr({
+	src: 'secrets.json',
+	onsuccess: function () {
+		var slugs = JSON.parse(this.responseText);
+		
+		var hash = slugs[slug];
+		
+		if (hash) {
+			// Redirect
+			var url = 'http://dabblet.com/gist/' + hash;
+			$('section.redirecting > p').innerHTML = 'Redirecting to <a href="' + url + '">' + url + '</a>';
+		}
+		else {
+			document.body.className = 'error not-found';
+		}
+	},
+	onerror: function () {
+		document.body.className = 'error json';
+	}
+});
+
+})();
